8c9dd9d4cd87f89f00fcfbb608d9d7146dc57177
liuzhen88 <l458861320@qq.com>
2016-09-10 23:04:30 +0800

    modify config

---
+++ config/config.js
@@ -0,0 +1,19 @@
+var config = {
+	success:{
+		code:"200",
+		message:"ok"
+	},
+	lack:{
+		code:"400",
+		message:"缺少参数"
+	},
+	loginTimeOut:{
+		code:"80001",
+		message:"登录超时"
+	},
+	error:{
+		code:"80002",
+		message:"操作错误"
+	}
+};
+module.exports = config;
